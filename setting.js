5aef0fbde89bf13678373e6dfe3b0e02df6fb05e
sendyhtml <sendyfalisha@gmail.com>
2026-01-01 07:15:35 +0700

    Add setting.js

---
+++ setting.js
@@ -0,0 +1,38 @@
+// Konfigurasi untuk Bot Telegram
+global.botToken = '8123980358:AAEdD3_4gaaH-2D1LwCDPMR4pRFxe8Q1DE0'; // <-- GANTI DENGAN TOKEN DARI BOTFATHER
+global.ownerIds = ['7546117130', '7546117130']; // <-- GANTI DENGAN ID TELEGRAM OWNER BOT, bisa lebih dari 1
+
+// Konfigurasi Global Lainnya
+global.author = 'SENDYHOST';
+
+// =================== KONFIGURASI MULTI-PANEL ===================
+global.panels = {
+    "panel1": {
+        name: "Panel 1",
+        url: 'https://',
+        ptla: 'ptla_',
+        ptlc: 'ptlc_', // <-- PENTING: Client API Key, diperlukan untuk mode hapus 'inactive'
+        egg: '15',
+        nest: '5',
+        loc: '1'
+    },
+    "panel2": {
+        name: "Panel 2",
+        url: 'https://',
+        ptla: 'ptla_',
+        ptlc: 'ptlc_',// <-- PENTING: Client API Key, diperlukan untuk mode hapus 'inactive'
+        egg: '15',
+        nest: '5',
+        loc: '1'
+    },
+    "panel3": {
+        name: "Panel 3",
+        url: 'https:/',
+        ptla: 'ptla_',
+        ptlc: 'ptlc_', // <-- PENTING: Client API Key, diperlukan untuk mode hapus 'inactive'
+        egg: '15',
+        nest: '5',
+        loc: '1'
+    }
+};
+// ===============================================================
